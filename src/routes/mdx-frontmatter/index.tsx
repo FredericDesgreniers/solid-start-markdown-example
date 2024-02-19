7a6e8dc8f35a26db0d2e2426435775fdf53e1c00
Frederic Desgreniers <fredericdesgreniers@gmail.com>
2024-02-18 19:32:24 -0500

    Fix link pointing to the wrong place

--- src/routes/mdx-frontmatter/index.tsx
+++ src/routes/mdx-frontmatter/index.tsx
@@ -4,7 +4,7 @@ import * as Example from "./example.mdx";
 export default function () {
   return (
     <>
-      <A href="./example.mdx">{Example.title}</A>
+      <A href="./example">{Example.title}</A>
       <h1>The content can still be rendered inline using `.default`</h1>
       <Example.default />
     </>

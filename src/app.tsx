4f9a1530171ba1c49dd96edbde7f5a145b01d77a
Frederic Desgreniers <fredericdesgreniers@gmail.com>
2024-02-18 19:31:03 -0500

    Move links to the layout layer in order to show them on every page

--- src/app.tsx
+++ src/app.tsx
@@ -1,5 +1,5 @@
 // @refresh reload
-import { Router } from "@solidjs/router";
+import { A, Router } from "@solidjs/router";
 import { FileRoutes } from "@solidjs/start";
 import { Suspense } from "solid-js";
 import "./app.css";
@@ -9,6 +9,26 @@ export default function App() {
     <Router
       root={(props) => (
         <main>
+          <ul>
+            <li>
+              <A href="/mdx-as-component">
+                Importing a markdown file as a component
+              </A>
+            </li>
+            <li>
+              <A href="/mdx-as-page">Rendering a markdown file as a page</A>
+            </li>
+            <li>
+              <A href="/mdx-frontmatter">
+                Extracting frontmatter from a MDX file
+              </A>
+            </li>
+            <li>
+              <A href="/mdx-code-highlighting">
+                Syntax highlighting code blocks
+              </A>
+            </li>
+          </ul>
           <Suspense>{props.children}</Suspense>
         </main>
       )}
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,24 +1,5 @@
 import { A } from "@solidjs/router";
 
 export default function () {
-  return (
-    <>
-      <ul>
-        <li>
-          <A href="/mdx-as-component">
-            Importing a markdown file as a component
-          </A>
-        </li>
-        <li>
-          <A href="/mdx-as-page">Rendering a markdown file as a page</A>
-        </li>
-        <li>
-          <A href="/mdx-frontmatter">Extracting frontmatter from a MDX file</A>
-        </li>
-        <li>
-          <A href="/mdx-code-highlighting">Syntax highlighting code blocks</A>
-        </li>
-      </ul>
-    </>
-  );
+  return <></>;
 }
